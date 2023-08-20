6f1879114efc315443631071a99635f294ab6bcb
Md-Abraar <94089831+Md-Abraar@users.noreply.github.com>
2023-08-20 15:08:42 +0530

    Update index.jsx
    Fixed headings, added border properties

--- client/src/scenes/DetailReport/index.jsx
+++ client/src/scenes/DetailReport/index.jsx
@@ -7,20 +7,20 @@ const DetailReport = () => {
     const { stateName } = useParams();
     return (
         <>
-            <div style={{padding: "30px", boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)', margin: "30px"}}>
+            <div style={{padding: "30px", boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)', margin: "30px", borderRadius: "10px", border: "2px solid rgba(54, 162, 235, 1)"}}>
             <center><h3>Cultivated land over the years (in hectares)</h3></center>
             <FirstGraph stateName={stateName} />
             </div>
-            <div style={{padding: "30px", boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)', margin: "30px"}}>
+            <div style={{padding: "30px", boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)', margin: "30px", borderRadius: "10px", border: "2px solid rgba(72,191,132,1)"}}>
             <center><h3>Total production over the years (in tonnes)</h3></center>
             <SecondGraph stateName={stateName} />
             </div>
-            <div style={{padding: "30px", boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)', margin: "30px"}}>
-            <center><h3>Total production over the years (in tonnes)</h3></center>
+            <div style={{padding: "30px", boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)', margin: "30px", borderRadius: "10px", border: "2px solid rgba(255,205,86,1)"}}>
+            <center><h3>Different Crops produced</h3></center>
             <ThirdGraph stateName={stateName} />
             </div>
-            <div style={{padding: "30px", boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)', margin: "30px"}}>
-            <center><h3>Total production over the years (in tonnes)</h3></center>
+            <div style={{padding: "30px", boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)', margin: "30px", borderRadius: "10px", border: "2px solid rgba(255,99,132,1)"}}>
+            <center><h3>Season wise cultivation</h3></center>
             <FourthGraph stateName={stateName} />
             </div>
         </>
