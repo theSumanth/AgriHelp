b26bcc22d5d62e480b11674cca4fa8e9c096fd63
parshu-8203 <parshu.yerranagu@gmail.com>
2023-08-29 14:59:11 +0530

    fertilizer output updated

--- client/src/components/CalculatorFrom3.jsx
+++ client/src/components/CalculatorFrom3.jsx
@@ -5,23 +5,24 @@ import potassium from './icons/potassium.png';
 import phosphorus from './icons/phosphorus.png';
 import soilicon from './icons/soil.png';
 import wheat from './icons/wheat.png';
-
+import axios from "axios";
 const CalculatorForm3 = () => {
-    const lands = [
-        "land1",
-        "land2",
-        "land3",
-    ]
+    // const lands = [
+    //     "land1",
+    //     "land2",
+    //     "land3",
+    // ]
 
     const soils = [
         "Black", "Clayey", "Loamy", "Red", "Sandy",
     ]
 
-    const crops =["Barley", "Cotton", "Ground Nuts", "Maize", "Millets", "Oil seeds", "Paddy", "Pulses", "Sugarcane","Tobacco","Wheat"]
+    const crops = ["Barley", "Cotton", "Ground Nuts", "Maize", "Millets", "Oil seeds", "Paddy", "Pulses", "Sugarcane", "Tobacco", "Wheat"]
 
-    const [nitro, setNitro] = useState("");
-    const [potas, setPotas] = useState("");
-    const [phos, setPhos] = useState("");
+    const [nitro, setNitro] = useState(0);
+    const [data, setData] = useState(0);
+    const [potas, setPotas] = useState(0);
+    const [phos, setPhos] = useState(0);
     const [soil, setSoil] = useState("");
     const [crop, setCrop] = useState("");
 
@@ -47,88 +48,101 @@ const CalculatorForm3 = () => {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log(nitro, soil, crop);
+        axios.post('http://localhost:5000/calculator', {
+            'nitro': nitro,
+            'potas': potas,
+            'phos': phos,
+            'soil': soil,
+            'crop': crop
+        })
+            .then(function(response)  { 
+                console.log(response.data.prediction);
+                setData(response.data.prediction);
+             })
+            .catch(error => console.error(error));
+
+        console.log(nitro, potas, phos, soil, crop);
     };
 
     return (
         <div className='firstform'>
             <form onSubmit={handleSubmit}>
                 <div className='form-input'>
-                <img src={nitrogen} alt='nitrogen icon'></img>
-                <input 
-                    className='calc-input-field' 
-                    type='text' 
-                    placeholder='Enter Nitrogen amount' 
-                    id='nitro' name='nitro' 
-                    onChange={onChangeHandler1}
-                />
+                    <img src={nitrogen} alt='nitrogen icon'></img>
+                    <input
+                        className='calc-input-field'
+                        type='text'
+                        placeholder='Enter Nitrogen amount'
+                        id='nitro' name='nitro'
+                        onChange={onChangeHandler1}
+                    />
                 </div>
                 <br />
 
                 <div className='form-input'>
-                <img src={potassium} alt='potassium icon'></img>
-                <input 
-                    className='calc-input-field' 
-                    type='text' 
-                    placeholder='Enter Potassium amount' 
-                    id='nitro' name='nitro' 
-                    onChange={onChangeHandler2}
-                />
+                    <img src={potassium} alt='potassium icon'></img>
+                    <input
+                        className='calc-input-field'
+                        type='text'
+                        placeholder='Enter Potassium amount'
+                        id='nitro' name='nitro'
+                        onChange={onChangeHandler2}
+                    />
                 </div>
                 <br />
 
                 <div className='form-input'>
-                <img src={phosphorus} alt='phosphorus icon'></img>
-                <input 
-                    className='calc-input-field' 
-                    type='text' 
-                    placeholder='Enter Phosphorus amount' 
-                    id='nitro' name='nitro' 
-                    onChange={onChangeHandler3}
-                />
+                    <img src={phosphorus} alt='phosphorus icon'></img>
+                    <input
+                        className='calc-input-field'
+                        type='text'
+                        placeholder='Enter Phosphorus amount'
+                        id='nitro' name='nitro'
+                        onChange={onChangeHandler3}
+                    />
                 </div>
-                <br />          
+                <br />
 
                 <div className='form-input'>
-                <img src={soilicon} alt='soil icon'></img>
-                <select
-                    className="calc-dropdown"
-                    onChange={onChangeHandler4}
-                    id='soils'
-                >
-                    <option value="none" selected disabled>Select soil</option>
-                    {soils.map((soil_el, index) => {
-                        return (
-                            <option key={index} value={soil_el}>
-                                {soil_el}
-                            </option>
-                        );
-                    })}
-                </select>
+                    <img src={soilicon} alt='soil icon'></img>
+                    <select
+                        className="calc-dropdown"
+                        onChange={onChangeHandler4}
+                        id='soils'
+                    >
+                        <option value="none" selected disabled>Select soil</option>
+                        {soils.map((soil_el, index) => {
+                            return (
+                                <option key={index} value={soil_el}>
+                                    {soil_el}
+                                </option>
+                            );
+                        })}
+                    </select>
                 </div>
 
-                <br />               
+                <br />
 
                 <div className='form-input'>
-                <img src={wheat} alt='crop icon'></img>
-                <select
-                    className="calc-dropdown"
-                    onChange={onChangeHandler5}
-                    id='crops'
-                >
-                    <option value="none" selected disabled>Select crop</option>
-                    {crops.map((crop_el, index) => {
-                        return (
-                            <option key={index} value={crop_el}>
-                                {crop_el}
-                            </option>
-                        );
-                    })}
-                </select>
+                    <img src={wheat} alt='crop icon'></img>
+                    <select
+                        className="calc-dropdown"
+                        onChange={onChangeHandler5}
+                        id='crops'
+                    >
+                        <option value="none" selected disabled>Select crop</option>
+                        {crops.map((crop_el, index) => {
+                            return (
+                                <option key={index} value={crop_el}>
+                                    {crop_el}
+                                </option>
+                            );
+                        })}
+                    </select>
                 </div>
 
-                <br />               
-              
+                <br />
+
                 <center><button className="button-40" type="submit" value="Submit">Submit</button></center>
             </form>
         </div>
--- client/src/scenes/Calculator/index.jsx
+++ client/src/scenes/Calculator/index.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import {React,useState, useEffect} from "react";
 import './calculator.css'
 import CalculatorMain from "../../components/CalculatorMain";
 import CalculatorMain2 from "../../components/CalculatorMain2";
 import bgvid from "../../widgets/bgvid2.mp4";
 import calc_bg from "../../components/images/calc_bg.jpg";
 import CalculatorMain3 from "../../components/CalculatorMain3";
-
+import axios from "axios";
 const Calculator = () => {
+    // const [data, setData] = useState('');
+
+    // useEffect(() => {
+    //     axios.get('http://localhost:5000/calculator')
+    //         .then(response => setData(response.data.message))
+    //         .catch(error => console.error(error));
+    //     console.log(data);
+    // }, []);
+
+ 
     return (
         <div className="parshu">
             <video className='calc-bg' muted autoPlay loop poster={calc_bg} id="bgvid">
